16ba83afb982bb3fbe1a8a38671d5fdfc7307421
Wes Payne <noblepayne@noblepayne.com>
2025-10-25 15:31:06 -0700

    chore: update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # workaround so devenv+flakes pick up .env when added to .gitignore
 # from: https://mtlynch.io/notes/use-nix-flake-without-git/
-nix develop --impure path:.
+NIXPKGS_ALLOW_UNFREE=1 nix develop --impure path:. -c $SHELL
